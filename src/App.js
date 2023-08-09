3df712a9a9dff1afdfbe6075773e60987fe4e396
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-09 12:30:52 +0530

    Dividing the Interface into components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Facts Office Logo" />
-        <h1>Facts Studio</h1>
-      </div>
-      <button className="btn btn-large btn-share">Share a fact</button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Facts Office Logo" />
+          <h1>Facts Studio</h1>
+        </div>
+        <button className="btn btn-large btn-share">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <FilterCategory />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">New Fact Form here..</form>;
+}
+
+function FilterCategory() {
+  return <aside>Categories here</aside>;
+}
+
+function FactList() {
+  return <section>List of facts goes here..</section>;
 }
 
 export default App;
