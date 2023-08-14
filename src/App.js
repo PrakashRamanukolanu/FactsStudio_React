a09cd2ac4613669f378b15402bd11a74701e0b06
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-14 10:49:05 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -13,41 +13,6 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text:
-      "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 function Loader() {
   return <p className="message">Loading...</p>;
 }
@@ -57,14 +22,18 @@ function App() {
   const [facts, setFacts] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(function() {
-    setLoading(true);
+  useEffect(function () {
     async function getFacts() {
+      setLoading(true);
+
       const { data: facts, error } = await supabase
         .from("facts")
         .select("*")
-        .order("votes_like", { ascending: false });
-      setFacts(facts);
+        .order("votes_like", { ascending: false })
+        .limit(20);
+
+      if (!error) setFacts(facts);
+      else alert("There was a problem loading the data");
       setLoading(false);
     }
     getFacts();
